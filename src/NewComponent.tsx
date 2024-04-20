e14bd8a9d886a298940616301672027b2f51f0d7
Dmitry Naumov <komrad.nda@yandex.ru>
2024-04-20 11:46:00 +0300

    ДЗ Map()

--- src/NewComponent.tsx
+++ src/NewComponent.tsx
@@ -1,29 +1,70 @@
 import React from 'react'
+//
+// type NewComponentPropsType = {
+// 	students: Array<StudentType>
+// 	/*или так*/
+// 	// students: StudentType[]
+// }
+//
+// type StudentType = {
+// 	id: number,
+// 	name: string,
+// 	age: number
+// }
+//
+// export const NewComponent = (props: NewComponentPropsType) => {
+// 	return (
+// 			<ul>
+// 				{props.students.map((objectFromStudentArray, index) => {
+// 					return (
+// 							<li key={objectFromStudentArray.id}>
+// 								<span>{objectFromStudentArray.name}</span>
+// 								<span> age: {objectFromStudentArray.age}</span>
+// 							</li>
+// 					)
+// 				})}
+// 			</ul>
+// 	)
+// }
+//
+
 
 type NewComponentPropsType = {
-	students: Array<StudentType>
+	topCars: Array<TopCarsType>
 	/*или так*/
 	// students: StudentType[]
 }
 
-type StudentType = {
-	id: number,
-	name: string,
-	age: number
+type TopCarsType = {
+	manufacturer: string,
+	model: string
 }
 
 export const NewComponent = (props: NewComponentPropsType) => {
 	return (
-			<ul>
-				{props.students.map((objectFromStudentArray, index) => {
-					return (
-							<li key={objectFromStudentArray.id}>
-								<span>{objectFromStudentArray.name}</span>
-								<span> age: {objectFromStudentArray.age}</span>
-							</li>
-					)
-				})}
-			</ul>
+			<table>
+				<tr>
+					{props.topCars.map((objectFromTopCarsArray, index) => {
+						return (
+								<td key={objectFromTopCarsArray.model}>{index + 1}</td>
+						)
+					})}
+				</tr>
+				<tr>
+					{props.topCars.map((objectFromTopCarsArray, index) => {
+						return (
+								<td key={objectFromTopCarsArray.model}>{objectFromTopCarsArray.manufacturer}</td>
+						)
+					})}
+				</tr>
+				<tr>
+					{props.topCars.map((objectFromTopCarsArray, index) => {
+						return (
+								<td key={objectFromTopCarsArray.model}>{objectFromTopCarsArray.model}</td>
+						)
+					})}
+				</tr>
+			</table>
 	)
-}
 
+}
