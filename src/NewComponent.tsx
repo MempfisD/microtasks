0c91f298814cb9a85112ca85831bf28191e5377f
Dmitry Naumov <komrad.nda@yandex.ru>
2024-04-21 14:27:32 +0300

    ДЗ Map() fix_1

--- src/NewComponent.tsx
+++ src/NewComponent.tsx
@@ -1,70 +1,28 @@
-import React from 'react'
-//
-// type NewComponentPropsType = {
-// 	students: Array<StudentType>
-// 	/*или так*/
-// 	// students: StudentType[]
-// }
-//
-// type StudentType = {
-// 	id: number,
-// 	name: string,
-// 	age: number
-// }
-//
-// export const NewComponent = (props: NewComponentPropsType) => {
-// 	return (
-// 			<ul>
-// 				{props.students.map((objectFromStudentArray, index) => {
-// 					return (
-// 							<li key={objectFromStudentArray.id}>
-// 								<span>{objectFromStudentArray.name}</span>
-// 								<span> age: {objectFromStudentArray.age}</span>
-// 							</li>
-// 					)
-// 				})}
-// 			</ul>
-// 	)
-// }
-//
-
+import React from 'react';
 
 type NewComponentPropsType = {
-	topCars: Array<TopCarsType>
-	/*или так*/
-	// students: StudentType[]
+	topCars: Array<CarsType>
 }
 
-type TopCarsType = {
-	manufacturer: string,
+type CarsType = {
+	manufacturer: string
 	model: string
 }
 
 export const NewComponent = (props: NewComponentPropsType) => {
-	return (
-			<table>
-				<tr>
-					{props.topCars.map((objectFromTopCarsArray, index) => {
-						return (
-								<td key={objectFromTopCarsArray.model}>{index + 1}</td>
-						)
-					})}
-				</tr>
-				<tr>
-					{props.topCars.map((objectFromTopCarsArray, index) => {
-						return (
-								<td key={objectFromTopCarsArray.model}>{objectFromTopCarsArray.manufacturer}</td>
-						)
-					})}
-				</tr>
-				<tr>
-					{props.topCars.map((objectFromTopCarsArray, index) => {
-						return (
-								<td key={objectFromTopCarsArray.model}>{objectFromTopCarsArray.model}</td>
-						)
-					})}
-				</tr>
-			</table>
-	)
-
-}
+			return (
+					<table>
+						<tbody>
+						{props.topCars.map((cars, index) => (
+								<tr key={cars.manufacturer + cars.model}>
+									<td>{index + 1}</td>
+									<td>{cars.manufacturer}</td>
+									<td>{cars.model}</td>
+								</tr>
+						))}
+						</tbody>
+					</table>
+			)
+					;
+		}
+;
